081683eaa99b77ac5ced7e0fc41d7032a3672ded
esmeramos <42855368+esmeramos@users.noreply.github.com>
2018-09-29 09:52:07 -0500

    Add files via upload

---
+++ query de inserts muebleria.sql
@@ -0,0 +1,64 @@
+insert into MetodoEnvio
+values (03, 'Estándar', 'Entrega entre 3 y 5 días hábiles')
+
+insert into MetodoEnvio
+values (02, 'Dos dias', 'Entrega en 2 días hábiles')
+
+insert into MetodoEnvio
+values (01, 'Un día', 'Entrega en 1 día hábil')
+
+insert into Departamento
+values(100, 'Línea Blanca')
+
+insert into Departamento
+values(101, 'Electrodomésticos')
+
+insert into Departamento
+values(102, 'Cómputo y Electrónica')
+
+insert into Departamento
+values(103, 'Juguetes')
+
+insert into Departamento
+values(104, 'Celulares')
+
+insert into Departamento
+values(105, 'Decoración')
+
+insert into Departamento
+values(106, 'Patio')
+
+insert into Departamento
+values(107, 'Baño')
+
+insert into Departamento
+values(108, 'Cocina')
+
+insert into Area_Trabajo 
+values (521, 'Ventas')
+
+insert into Area_Trabajo 
+values (522, 'Recursos Humanos')
+
+insert into Area_Trabajo 
+values (523, 'Seguridad')
+
+insert into Area_Trabajo 
+values (524, 'Gerencia')
+
+insert into Pago
+values (200, 'Efectivo')
+
+insert into Pago
+values (201, 'Tarjeta de débito')
+
+insert into Pago
+values (202, 'Tarjeta de crédito')
+
+insert into Turno
+values (001, 'Día', 8:00, 17:00)
+
+insert into Turno 
+values (002, 'Tarde', 13:00, 22:00)
+
+insert into 
